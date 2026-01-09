6191a66a0b09454f551fa0cf164a9bc6a19ca4a4
lalchhabi <tmgchhabi555@gmail.com>
2026-01-09 09:28:14 +0545

    build prompt and generate answer using gemma model from hf

--- llm/generator.py
+++ llm/generator.py
@@ -6,5 +6,14 @@ def generate_answer(prompt, max_tokens = 300):
 
     with torch.no_grad():
         outputs = model.generate(
-            
+            **inputs,
+            max_new_tokens = max_tokens,
+            temperature = 0.2,
+            top_p = 0.9,
+            do_sample = True
         )
+
+    answer = tokens.decode(
+        outputs[0], skip_special_tokens = True
+    )
+    return answer
