076328301fa9d4c4cd173fa51647c45be56ec832
lalchhabi <tmgchhabi555@gmail.com>
2026-05-17 08:29:50 +0545

    feat:migrate llm model load from local model to hugging face using Inference API

--- llm/hf_model.py
+++ llm/hf_model.py
@@ -1,43 +1,26 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch 
+### import libraries
+import os
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 
-# Pre-trained model name (instruction-tuned model)
-model_name = "google/gemma-2b-it"
+### Load environment variables 
+load_dotenv()
 
+HF_TOKEN = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
 
-def load_model():
+### Load model name
+MODEL_NAME = 'google/gemma-2-2b-it'
+
+def load_llm():
+    """Load Hugging Face LLM via Inference API
     """
-    Load HuggingFace tokenizer and language model.
+    llm = HuggingFaceEndpoint(
+        repo_id=MODEL_NAME,
+        huggingfacehub_api_token=HF_TOKEN,
+        temperature=0.1,
+        )
+    
+    chat_model = ChatHuggingFace(llm = llm)
+    
+    return chat_model
 
-    This function initializes the LLM used in the RAG pipeline.
-    It loads both:
-    - Tokenizer → converts text into tokens
-    - Model → generates responses from tokens
-
-    Returns
-    -------
-    tuple
-        tokenizer : AutoTokenizer
-            Tokenizer for encoding/decoding text.
-
-        model : AutoModelForCausalLM
-            Pre-trained causal language model for text generation.
-
-    Notes
-    -----
-    - Uses half precision (float16) for faster inference
-    - Uses automatic device mapping (CPU/GPU)
-    - Optimized for local LLM inference
-    """
-
-    # Load tokenizer (text → tokens)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-    # Load model with optimized settings
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        torch_dtype=torch.float16,   # Use FP16 for faster performance
-        device_map="auto"            # Automatically use GPU if available
-    )
-
-    return tokenizer, model
